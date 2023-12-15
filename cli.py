c438723c32807a7b35e1ca6a851402346bdcf45d
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-12-15 10:14:09 +0300

    Complete

--- cli.py
+++ cli.py
@@ -147,5 +147,39 @@ def add_roomtype(Roomtype, Roomprice, status):
     click.echo("Room Type added successfully.")
 
 
+@cli.command()
+@click.option("--orderid", prompt="Order Id", help="Order Id")
+@click.option("--itemid", prompt="Item Id", help="Item Id")
+@click.option("--bookid", prompt="Book Id", help="Book Id")
+@click.option("--orderdate", prompt="Order Date", help="Order Date")
+@click.option("--quantity", prompt="Quantity", help="Quantity")
+@click.option("--cost", prompt="Cost", help="Cost")
+@click.option("--status", default="active", help="Order's Status (default: active)")
+def add_customer(orderid, itemid, bookid, orderdate, quantity, cost, status):
+    try:
+        # Remove leading and trailing spaces from orderdate
+        orderdate = orderdate.strip()
+        
+        orderdate = datetime.strptime(orderdate, "%Y-%m-%d %H:%M:%S")
+        quantity = int(quantity)
+        cost = float(cost)
+    except ValueError as e:
+        click.echo(f"Error: {e}")
+        return
+
+    order = Order(
+        orderid=orderid,
+        itemid=itemid,
+        bookid=bookid,
+        orderdate=orderdate,
+        quantity=quantity,
+        cost=cost,
+        status=status
+    )
+    session.add(order)
+    session.commit()
+    click.echo("Order added successfully.")
+
+
 if __name__ == "__main__":
     cli()
